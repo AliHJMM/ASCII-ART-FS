7013fe9baeb99ce8db3b3c7d8c8482d49a7972f3
alihasan6 <AliHasanJMM@gmail.com>
2024-06-22 13:40:55 +0000

    Added CharValidation Function

--- main.go
+++ main.go
@@ -121,3 +121,12 @@ func ascii_fs(args []string) {
 	}
 }
 
+func charValidation(str string) bool {
+	slice := []rune(str)
+	for _, char := range slice {
+		if char < 32 || char > 126 {
+			return false
+		}
+	}
+	return true
+}
